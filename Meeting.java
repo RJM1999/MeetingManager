f916d18c0270cbebd060475c7538aee1d57250e5
RJM1999 <33182930+RJM1999@users.noreply.github.com>
2018-03-21 15:16:32 +0000

    Meeting and Diary

---
+++ Meeting.java
@@ -0,0 +1,130 @@
+import java.util.Date;
+
+public class Meeting
+{
+	//Define Fields
+	Date startTime;
+	Date endTime;
+	String description;
+	Meeting left;
+	Meeting right;
+	
+	/**
+	 *Constructor, no parameters
+	 */
+	Meeting()
+	{
+		
+	}
+	
+	Meeting(Date inStartTime, String desc)
+	{
+		startTime = inStartTime;
+		description = desc;
+	}
+	
+	Meeting(Date inStartTime, Date inEndTime, String desc)
+	{
+		startTime = inStartTime;
+		endTime = inEndTime;
+		description = desc;
+	}
+	
+	/**
+	 * Default constructor, with parameters. This would be the one normally used.
+	 * @param inDateOfMeeting - Date the Date of the meeting
+	 * @param inStartTime - Time start time of the meeting
+	 * @param inEndTime - Time end time of the meeting
+	 * @param inDescription - String a description of the meeting
+	 * @param inLeft - Meeting a pointer for the left node of meeting
+	 * @param inRight - Meeting a pointer for the right node of meeting
+	 */
+	Meeting(Date inEndTime, Date inStartTime,String inDescription, Meeting inLeft, Meeting inRight)
+	{
+		
+		startTime = inStartTime;
+		endTime = inEndTime;
+		description = inDescription;
+		left = inLeft;
+		right = inRight;
+	}
+	
+	public String getData()
+	{
+		String data = "The start date and time of the meeting is " + getStartTime() + " The end time and date of the meeting is " + getEndTime() + " Description: " + getDescription();
+		return data;
+	}
+	
+	//****GETTERS AND SETTERS****
+
+	/**
+	 * @return the startTime
+	 */
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	/**
+	 * @param startTime the startTime to set
+	 */
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	/**
+	 * @return the endTime
+	 */
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	/**
+	 * @param endTime the endTime to set
+	 */
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the left
+	 */
+	public Meeting getLeft() {
+		return left;
+	}
+
+	/**
+	 * @param left the left to set
+	 */
+	public void setLeft(Meeting left) {
+		this.left = left;
+	}
+
+	/**
+	 * @return the right
+	 */
+	public Meeting getRight() {
+		return right;
+	}
+
+	/**
+	 * @param right the right to set
+	 */
+	public void setRight(Meeting right) {
+		this.right = right;
+	}
+	
+}
